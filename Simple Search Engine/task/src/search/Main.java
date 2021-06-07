c87882546579b49f7d9569437535b549df953a9c
Christian Jairo Sarmiento <55115040+jairo8925@users.noreply.github.com>
2021-06-07 00:57:38 -0700

    initial commit

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -3,10 +3,44 @@ package search;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        List<String> dataset = new ArrayList<>();
+    static final Scanner scanner = new Scanner(System.in);
+    static final List<String> dataset = new ArrayList<>();
+
+    public static void main(String[] args) {
+        populateDataset();
+
+        while (true) {
+            System.out.println("=== Menu ===");
+            System.out.println("1. Search information.");
+            System.out.println("2. Print all people");
+            System.out.println("0. Exit");
+            String command = scanner.nextLine();
+            System.out.println();
+            switch (command) {
+                case "1":
+                    findPerson();
+                    break;
+                case "2":
+                    System.out.println(" === List of people ===");
+                    for (String data : dataset) {
+                        System.out.println(data);
+                    }
+                    System.out.println();
+                    break;
+                case "0":
+                    System.out.println("Bye!");
+                    scanner.close();
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Incorrect option! Try again.");
+                    System.out.println();
+            }
+        }
+    }
+
+    public static void populateDataset() {
         System.out.println("Enter the number of people:");
         int amount = scanner.nextInt();
         scanner.nextLine(); // consume input
@@ -15,29 +49,21 @@ public class Main {
             dataset.add(scanner.nextLine());
         }
         System.out.println();
+    }
 
-        System.out.println("Enter the number of search queries:");
-        amount = scanner.nextInt();
-        System.out.println();
-        scanner.nextLine(); // consume input
-        for (int i = 0; i < amount; i++) {
-            System.out.println("Enter data to search people:");
-            String query = scanner.next();
-            boolean found = false;
-            for(String data : dataset) {
-                if (data.toLowerCase().contains(query.toLowerCase())) {
-                    if (!found) {
-                        System.out.println();
-                        System.out.println("Found people:");
-                    }
-                    System.out.println(data);
-                    found = true;
-                }
+    public static void findPerson() {
+        System.out.println("Enter a name or email to search all suitable people.");
+        String query = scanner.nextLine();
+        boolean found = false;
+        for(String data : dataset) {
+            if (data.toLowerCase().contains(query.toLowerCase())) {
+                System.out.println(data);
+                found = true;
             }
-            if (!found) {
-                System.out.println("No matching people found.");
-            }
-            System.out.println();
         }
+        if (!found) {
+            System.out.println("No matching people found.");
+        }
+        System.out.println();
     }
 }
